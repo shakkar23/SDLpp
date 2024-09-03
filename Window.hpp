dffc7ac0809b4862f867188f02c9d6c125f2b4cb
Shakkar23 <shakkar23@gmail.com>
2024-09-03 16:25:43 -0700

    format code and make renderer publicly accesible if requested

--- Window.hpp
+++ Window.hpp
@@ -2,8 +2,8 @@
 
 #include <SDL2/SDL.h>
 
-#include <stack>
 #include <iostream>
+#include <stack>
 #include <string>
 #include <utility>
 
@@ -14,24 +14,25 @@ constexpr int UPDATES_PER_SECOND = UPS;
 #endif
 
 class Window {
-public:
- Window(const char* p_title, const int p_w, const int p_h);
- Window(const char* p_title, const double r_w, const double r_h);
- ~Window();
- // window related
- int getRefreshrate();
- std::pair<int, int> getWindowSize();
- void setWindowSize(int x, int y);
- // drawing related
- bool render(SDL_Rect src, SDL_Rect dst, SDL_Texture* tex);
+   public:
+    Window(const char* p_title, const int p_w, const int p_h);
+    Window(const char* p_title, const double r_w, const double r_h);
+    ~Window();
 
- void renderCopy(SDL_Texture* texture,
-                 const SDL_Rect* srcrect,
-                 const SDL_Rect* dstrect);
+    // window related
+    int getRefreshrate();
+    std::pair<int, int> getWindowSize();
+    void setWindowSize(int x, int y);
+    // drawing related
+    bool render(SDL_Rect src, SDL_Rect dst, SDL_Texture* tex);
 
- void display();
+    void renderCopy(SDL_Texture* texture,
+                    const SDL_Rect* srcrect,
+                    const SDL_Rect* dstrect);
 
-public:
+    void display();
+
+   public:
     void clear();
     void drawCircle(int x, int y, int r);
     void drawRect(SDL_Rect rec);
@@ -49,11 +50,12 @@ public:
 
     SDL_Rect getInnerRect(SDL_Rect parent, float aspect_ratio);
 
-private:
+
+    SDL_Renderer* getRenderer();
+   private:
     SDL_Window* window;
     SDL_Renderer* renderer;
 
-    SDL_Renderer* getRenderer();
 
     std::stack<SDL_Color> colors;
 };
@@ -66,40 +68,35 @@ inline Window::Window(const char* p_title, const int p_w, const int p_h)
     : window(NULL), renderer(NULL) {
     this->window =
         SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+                         SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
     if (this->window == NULL) {
         std::cout << "Window failed to init. Error: " << SDL_GetError() << std::endl;
     }
 
     renderer = SDL_CreateRenderer(
-        this->window, -1, SDL_RENDERER_ACCELERATED
-        | SDL_RendererFlags::SDL_RENDERER_PRESENTVSYNC
-    );
-    //SDL_RenderSetLogicalSize(this->renderer, DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT); 
+        this->window, -1, SDL_RENDERER_ACCELERATED | SDL_RendererFlags::SDL_RENDERER_PRESENTVSYNC);
+    // SDL_RenderSetLogicalSize(this->renderer, DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
 
-
-
-    SDL_Surface* surface;     // Declare an SDL_Surface to be filled in with pixel data from an image file
+    SDL_Surface* surface;  // Declare an SDL_Surface to be filled in with pixel data from an image file
 #define O 0x0000
 #define _ 0xffff
-    Uint16 pixels[16 * 16] = {  // ...or with raw pixel data:
-      _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _,
-      _, _, _, _, _, _, O, O, O, O, O, _, _, _, _, _,
-      _, _, _, _, _, O, _, _, _, _, _, O, _, _, _, _,
-      _, _, _, _, O, _, _, _, _, _, _, O, _, _, _, _,
-      _, _, _, _, O, _, _, _, _, _, O, _, O, _, _, _,
-      _, _, O, O, _, _, O, _, _, _, _, _, O, O, _, _,
-      _, _, _, O, _, _, _, _, O, O, _, _, O, _, _, _,
-      _, _, _, O, _, _, _, _, O, O, _, O, O, _, _, _,
-      _, _, _, O, _, _, _, _, _, _, _, O, _, _, _, _,
-      _, _, _, O, _, _, _, _, _, _, _, O, _, _, _, _,
-      _, _, _, _, O, _, _, _, _, O, O, _, _, _, _, _,
-      _, _, _, _, _, O, O, O, O, _, _, O, _, _, _, _,
-      _, _, _, _, _, O, _, _, _, _, _, O, O, _, _, _,
-      _, _, _, _, _, O, _, _, _, _, _, _, _, _, _, _,
-      _, _, _, _, O, O, _, _, _, _, _, _, _, _, _, _,
-      _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _
-    };
+    Uint16 pixels[16 * 16] = {// ...or with raw pixel data:
+                              _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _,
+                              _, _, _, _, _, _, O, O, O, O, O, _, _, _, _, _,
+                              _, _, _, _, _, O, _, _, _, _, _, O, _, _, _, _,
+                              _, _, _, _, O, _, _, _, _, _, _, O, _, _, _, _,
+                              _, _, _, _, O, _, _, _, _, _, O, _, O, _, _, _,
+                              _, _, O, O, _, _, O, _, _, _, _, _, O, O, _, _,
+                              _, _, _, O, _, _, _, _, O, O, _, _, O, _, _, _,
+                              _, _, _, O, _, _, _, _, O, O, _, O, O, _, _, _,
+                              _, _, _, O, _, _, _, _, _, _, _, O, _, _, _, _,
+                              _, _, _, O, _, _, _, _, _, _, _, O, _, _, _, _,
+                              _, _, _, _, O, _, _, _, _, O, O, _, _, _, _, _,
+                              _, _, _, _, _, O, O, O, O, _, _, O, _, _, _, _,
+                              _, _, _, _, _, O, _, _, _, _, _, O, O, _, _, _,
+                              _, _, _, _, _, O, _, _, _, _, _, _, _, _, _, _,
+                              _, _, _, _, O, O, _, _, _, _, _, _, _, _, _, _,
+                              _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _};
 #undef _
 #undef O
     surface = SDL_CreateRGBSurfaceFrom(pixels, 16, 16, 16, 32, 0x0f00, 0x00f0, 0x000f, 0xf000);
@@ -117,7 +114,7 @@ inline Window::Window(const char* p_title, const double r_w, const double r_h) {
 
     // call the other constructor
     auto tmp = Window(p_title, int(mode.w * r_w), int(mode.h * r_h));
-    
+
     *this = tmp;
 
     tmp.renderer = NULL;
@@ -139,7 +136,7 @@ inline int Window::getRefreshrate() {
 inline std::pair<int, int> Window::getWindowSize() {
     int x, y;
     SDL_GetWindowSize(this->window, &x, &y);
-    return { x, y };
+    return {x, y};
 }
 
 inline void Window::setWindowSize(int x, int y) {
@@ -149,7 +146,6 @@ inline void Window::setWindowSize(int x, int y) {
 // passing in a src with all zeros will grab the entire texture
 // returns weather it worked or not
 inline bool Window::render(SDL_Rect src, SDL_Rect dst, SDL_Texture* tex) {
-
     auto checkIfSet = [&](SDL_Rect box) {if ((box.x == 0) && (box.y == 0) && (box.w == 0) && (box.h == 0)) return true; else return false; };
 
     auto err = SDL_RenderCopy(this->renderer, tex, (checkIfSet(src)) ? NULL : &src, &dst);
@@ -159,13 +155,11 @@ inline bool Window::render(SDL_Rect src, SDL_Rect dst, SDL_Texture* tex) {
         return false;
     }
     return true;
-
 }
 
 inline void Window::renderCopy(SDL_Texture* texture, const SDL_Rect* srcrect, const SDL_Rect* dstrect) {
     SDL_RenderCopy(renderer, texture, srcrect, dstrect);
 }
-
 
 inline SDL_Renderer* Window::getRenderer() {
     return renderer;
@@ -184,8 +178,7 @@ inline void Window::drawCircle(int X, int Y, int r) {
     int32_t ty = 1;
     int32_t error = (tx - diameter);
 
-    while (x >= y)
-    {
+    while (x >= y) {
         //  Each of the following renders an octant of the circle
         SDL_Point points[8] = {
             {X + x, Y - y},
@@ -195,18 +188,16 @@ inline void Window::drawCircle(int X, int Y, int r) {
             {X + y, Y - x},
             {X + y, Y + x},
             {X - y, Y - x},
-            {X - y, Y + x} };
+            {X - y, Y + x}};
         SDL_RenderDrawPoints(renderer, points, 8);
 
-        if (error <= 0)
-        {
+        if (error <= 0) {
             ++y;
             error += ty;
             ty += 2;
         }
 
-        if (error > 0)
-        {
+        if (error > 0) {
             --x;
             tx += 2;
             error += (tx - diameter);
@@ -214,15 +205,11 @@ inline void Window::drawCircle(int X, int Y, int r) {
     }
 }
 
-
-
-inline void Window::drawRect(SDL_Rect rec)
-{
+inline void Window::drawRect(SDL_Rect rec) {
     SDL_RenderDrawRect(renderer, &rec);
 }
 
-inline void Window::drawRectFilled(SDL_Rect rec)
-{
+inline void Window::drawRectFilled(SDL_Rect rec) {
     SDL_RenderFillRect(renderer, &rec);
 }
 
@@ -230,8 +217,7 @@ inline SDL_Texture* Window::CreateTextureFromSurface(SDL_Surface* surface) {
     return SDL_CreateTextureFromSurface(this->renderer, surface);
 }
 
-inline SDL_Texture* Window::CreateTextureFromWindow()
-{
+inline SDL_Texture* Window::CreateTextureFromWindow() {
     auto surface = SDL_GetWindowSurface(window);
     auto texture = SDL_CreateTextureFromSurface(renderer, surface);
 
@@ -240,13 +226,11 @@ inline SDL_Texture* Window::CreateTextureFromWindow()
     return texture;
 }
 
-inline void Window::setDrawColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
-{
+inline void Window::setDrawColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
     SDL_SetRenderDrawColor(renderer, r, g, b, a);
 }
 
-inline void Window::getDrawColor(Uint8& r, Uint8& g, Uint8& b, Uint8& a)
-{
+inline void Window::getDrawColor(Uint8& r, Uint8& g, Uint8& b, Uint8& a) {
     SDL_GetRenderDrawColor(renderer, &r, &g, &b, &a);
 }
 
@@ -268,13 +252,12 @@ inline SDL_Rect Window::getInnerRect(SDL_Rect parent, float aspect_ratio) {
     if ((float)parent.w / parent.h > aspect_ratio) {
         height = std::min(parent.h, int(parent.w / aspect_ratio));
         width = int(height * aspect_ratio);
-    }
-    else {
+    } else {
         width = std::min(parent.w, int(parent.h * aspect_ratio));
         height = int(width / aspect_ratio);
     }
 
-    SDL_Rect board = { parent.x + (parent.w - width) / 2, parent.y + (parent.h - height) / 2, width, height };
+    SDL_Rect board = {parent.x + (parent.w - width) / 2, parent.y + (parent.h - height) / 2, width, height};
 
     return board;
 }
